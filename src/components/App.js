8f13846ea10d8efce10fcddf98666beba6f5fe49
Yashraj Jain <yashrajjain726@gmail.com>
2021-02-07 18:44:59 +0530

    data added and fetch locally

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
-
-
+import React from 'react'
+import NavBar from './NavBar'
+import {data} from '../data'
+import MovieCard from './MovieCard'
 function App() {
   return (
     <div className="App">
-     
+     <NavBar/>
+     <div className="tabs">
+       <div className="tab">Movies</div>
+       <div className="tab">Favourite</div>
+     </div>
+     <div className="list">
+       {data.map((movie)=>{
+         return <MovieCard movie={movie}/>
+       })}
+     </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,229 @@
+export const data =[{
+    "Title": "Superman",
+    "Year": "1978",
+    "Rated": "PG",
+    "Released": "15 Dec 1978",
+    "Runtime": "143 min",
+    "Genre": "Action, Adventure, Drama, Sci-Fi",
+    "Director": "Richard Donner",
+    "Writer": "Jerry Siegel (character created by: Superman), Joe Shuster (character created by: Superman), Mario Puzo (story), Mario Puzo (screenplay), David Newman (screenplay), Leslie Newman (screenplay), Robert Benton (screenplay)",
+    "Actors": "Marlon Brando, Gene Hackman, Christopher Reeve, Ned Beatty",
+    "Plot": "An alien orphan is sent from his dying planet to Earth, where he grows up to become his adoptive home's first and greatest superhero.",
+    "Language": "English",
+    "Country": "USA, UK, Canada, Switzerland",
+    "Awards": "Nominated for 3 Oscars. Another 16 wins & 20 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMzA0YWMwMTUtMTVhNC00NjRkLWE2ZTgtOWEzNjJhYzNiMTlkXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg",
+    "Ratings": [
+    {
+    "Source": "Internet Movie Database",
+    "Value": "7.3/10"
+    },
+    {
+    "Source": "Rotten Tomatoes",
+    "Value": "94%"
+    },
+    {
+    "Source": "Metacritic",
+    "Value": "80/100"
+    }
+    ],
+    "Metascore": "80",
+    "imdbRating": "7.3",
+    "imdbVotes": "158,915",
+    "imdbID": "tt0078346",
+    "Type": "movie",
+    "DVD": "N/A",
+    "BoxOffice": "$134,451,603",
+    "Production": "Dovemead Films",
+    "Website": "N/A",
+    "Response": "True"
+    },
+    {
+        "Title": "The Avengers",
+        "Year": "2012",
+        "Rated": "PG-13",
+        "Released": "04 May 2012",
+        "Runtime": "143 min",
+        "Genre": "Action, Adventure, Sci-Fi",
+        "Director": "Joss Whedon",
+        "Writer": "Joss Whedon (screenplay), Zak Penn (story), Joss Whedon (story)",
+        "Actors": "Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth",
+        "Plot": "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+        "Language": "English, Russian, Hindi",
+        "Country": "USA",
+        "Awards": "Nominated for 1 Oscar. Another 38 wins & 79 nominations.",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+        "Ratings": [
+        {
+        "Source": "Internet Movie Database",
+        "Value": "8.0/10"
+        },
+        {
+        "Source": "Rotten Tomatoes",
+        "Value": "91%"
+        },
+        {
+        "Source": "Metacritic",
+        "Value": "69/100"
+        }
+        ],
+        "Metascore": "69",
+        "imdbRating": "8.0",
+        "imdbVotes": "1,257,956",
+        "imdbID": "tt0848228",
+        "Type": "movie",
+        "DVD": "N/A",
+        "BoxOffice": "$623,357,910",
+        "Production": "Marvel Studios",
+        "Website": "N/A",
+        "Response": "True"
+        },
+        {
+            "Title": "Harry Potter and the Deathly Hallows: Part 2",
+            "Year": "2011",
+            "Rated": "PG-13",
+            "Released": "15 Jul 2011",
+            "Runtime": "130 min",
+            "Genre": "Adventure, Drama, Fantasy, Mystery",
+            "Director": "David Yates",
+            "Writer": "Steve Kloves (screenplay), J.K. Rowling (novel)",
+            "Actors": "Ralph Fiennes, Michael Gambon, Alan Rickman, Daniel Radcliffe",
+            "Plot": "Harry, Ron, and Hermione search for Voldemort's remaining Horcruxes in their effort to destroy the Dark Lord as the final battle rages on at Hogwarts.",
+            "Language": "English",
+            "Country": "UK, USA",
+            "Awards": "Nominated for 3 Oscars. Another 46 wins & 91 nominations.",
+            "Poster": "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
+            "Ratings": [
+            {
+            "Source": "Internet Movie Database",
+            "Value": "8.1/10"
+            },
+            {
+            "Source": "Rotten Tomatoes",
+            "Value": "96%"
+            },
+            {
+            "Source": "Metacritic",
+            "Value": "85/100"
+            }
+            ],
+            "Metascore": "85",
+            "imdbRating": "8.1",
+            "imdbVotes": "761,228",
+            "imdbID": "tt1201607",
+            "Type": "movie",
+            "DVD": "N/A",
+            "BoxOffice": "$381,409,310",
+            "Production": "Heyday Films, Moving Picture Company, Warner Bros.",
+            "Website": "N/A",
+            "Response": "True"
+            },{
+                "Title": "Die Hard",
+                "Year": "1988",
+                "Rated": "R",
+                "Released": "20 Jul 1988",
+                "Runtime": "132 min",
+                "Genre": "Action, Thriller",
+                "Director": "John McTiernan",
+                "Writer": "Roderick Thorp (based on the novel by), Jeb Stuart (screenplay by), Steven E. de Souza (screenplay by)",
+                "Actors": "Bruce Willis, Bonnie Bedelia, Reginald VelJohnson, Paul Gleason",
+                "Plot": "An NYPD officer tries to save his wife and several others taken hostage by German terrorists during a Christmas party at the Nakatomi Plaza in Los Angeles.",
+                "Language": "English, German, Italian, Japanese",
+                "Country": "USA",
+                "Awards": "Nominated for 4 Oscars. Another 8 wins & 2 nominations.",
+                "Poster": "https://m.media-amazon.com/images/M/MV5BZjRlNDUxZjAtOGQ4OC00OTNlLTgxNmQtYTBmMDgwZmNmNjkxXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+                "Ratings": [
+                {
+                "Source": "Internet Movie Database",
+                "Value": "8.2/10"
+                },
+                {
+                "Source": "Rotten Tomatoes",
+                "Value": "94%"
+                },
+                {
+                "Source": "Metacritic",
+                "Value": "72/100"
+                }
+                ],
+                "Metascore": "72",
+                "imdbRating": "8.2",
+                "imdbVotes": "790,935",
+                "imdbID": "tt0095016",
+                "Type": "movie",
+                "DVD": "N/A",
+                "BoxOffice": "$83,844,093",
+                "Production": "Silver Pictures, Gordon Company",
+                "Website": "N/A",
+                "Response": "True"
+                },
+                {
+"Title": "Yes",
+"Year": "2004",
+"Rated": "R",
+"Released": "05 Aug 2005",
+"Runtime": "100 min",
+"Genre": "Drama, Romance",
+"Director": "Sally Potter",
+"Writer": "Sally Potter",
+"Actors": "Shirley Henderson, Joan Allen, Sam Neill, Simon Abkarian",
+"Plot": "In this film, told almost entirely in iambic pentameter, She is a scientist in a loveless marriage to Anthony, a devious politician. He is a Lebanese doctor in self-imposed exile, working ...",
+"Language": "English",
+"Country": "UK, USA",
+"Awards": "2 wins & 3 nominations.",
+"Poster": "https://m.media-amazon.com/images/M/MV5BMTMxNzAyMTg4NV5BMl5BanBnXkFtZTcwNjUwMTkyMQ@@._V1_SX300.jpg",
+"Ratings": [
+{
+"Source": "Internet Movie Database",
+"Value": "6.7/10"
+},
+{
+"Source": "Rotten Tomatoes",
+"Value": "52%"
+},
+{
+"Source": "Metacritic",
+"Value": "55/100"
+}
+],
+"Metascore": "55",
+"imdbRating": "6.7",
+"imdbVotes": "3,025",
+"imdbID": "tt0381717",
+"Type": "movie",
+"DVD": "N/A",
+"BoxOffice": "$396,760",
+"Production": "Adventure Pictures, The Film Council, GreeneStreet/SKE Films",
+"Website": "N/A",
+"Response": "True"
+},
+{
+    "Title": "13 Reasons Why",
+    "Year": "2017–2020",
+    "Rated": "TV-MA",
+    "Released": "31 Mar 2017",
+    "Runtime": "60 min",
+    "Genre": "Drama, Mystery, Thriller",
+    "Director": "N/A",
+    "Writer": "Brian Yorkey",
+    "Actors": "Dylan Minnette, Christian Navarro, Alisha Boe, Brandon Flynn",
+    "Plot": "Follows teenager Clay Jensen, in his quest to uncover the story behind his classmate and crush, Hannah, and her decision to end her life.",
+    "Language": "English",
+    "Country": "USA",
+    "Awards": "Nominated for 1 Golden Globe. Another 4 wins & 14 nominations.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMDYzZTRlNGEtZDc2Mi00ZGNjLTlmZDAtMmVjMDZkOThiODEwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_SX300.jpg",
+    "Ratings": [
+    {
+    "Source": "Internet Movie Database",
+    "Value": "7.6/10"
+    }
+    ],
+    "Metascore": "N/A",
+    "imdbRating": "7.6",
+    "imdbVotes": "255,448",
+    "imdbID": "tt1837492",
+    "Type": "series",
+    "totalSeasons": "4",
+    "Response": "True"
+    }
+]
